801b64eac0271e6847afd1e13dfc8d842df2e3ae
NinjiClaw <andyv@inbox.ru>
2023-09-11 15:00:58 +0500

    project start

---
+++ src/main.js
@@ -0,0 +1,20 @@
+/**
+ * main.js - главная точка входа (entrypoint)
+ * Именно в этом файле запускается и инициализируется всё приложение
+ */
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import { router } from './router/index.js';
+import { createPinia } from 'pinia';
+
+const pinia = createPinia();
+
+// TODO: установить плагины: router, pinia, head(title), toaster, progress
+createApp(App)
+.use(router)
+.use(pinia)
+.mount('#app');
+
+// В этом же файле при необходимости можно сделать всё, что требуется делать ещё до создания приложения,
+// или что не относится к Vue приложению и UI
