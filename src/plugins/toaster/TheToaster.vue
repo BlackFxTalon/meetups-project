45e30d7e4de058c0efea61da2dfd8a947faa8d7c
NinjiClaw <andyv@inbox.ru>
2023-09-12 23:50:06 +0500

    компоненты для тостера

--- src/plugins/toaster/TheToaster.vue
+++ src/plugins/toaster/TheToaster.vue
@@ -12,8 +12,8 @@
 </template>
 
 <script>
-import UiToastList from './UiToastList.vue';
-import UiToast from './UiToast.vue';
+import UiToastList from '@/components/UiToastList.vue';
+import UiToast from '@/components/UiToast.vue';
 
 export default {
   name: 'TheToaster',
@@ -62,55 +62,3 @@ export default {
 
 };
 </script>
-
-
-<style scoped>
-/* _toaster.css */
-.toasts {
-  position: fixed;
-  bottom: 8px;
-  right: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  white-space: pre-wrap;
-  z-index: 999;
-}
-
-@media all and (min-width: 992px) {
-  .toasts {
-    bottom: 72px;
-    right: 112px;
-  }
-}
-
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
-
-.toast + .toast {
-  margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
-}
-</style>
