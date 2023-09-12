54cb84ece8e37bce70a0825abe8af53b0dec5c48
NinjiClaw <andyv@inbox.ru>
2023-09-12 23:52:30 +0500

    плагины router, pinia, head(title), toaster, progress

--- src/main.js
+++ src/main.js
@@ -7,14 +7,23 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router/index.js';
 import { createPinia } from 'pinia';
+import { createToaster } from './plugins/toaster';
+import { createProgress } from './plugins/progress';
+import { createHeadTitle } from './plugins/headTitle';
 
 const pinia = createPinia();
+const toaster = createToaster({ container: '#toaster' });
+const progress = createProgress({ container: '#progress', router });
+const headTitle = createHeadTitle();
 
-// TODO: установить плагины: router, pinia, head(title), toaster, progress
 createApp(App)
 .use(router)
 .use(pinia)
+.use(headTitle)
+.use(toaster)
+.use(progress)
 .mount('#app');
+
 
 // В этом же файле при необходимости можно сделать всё, что требуется делать ещё до создания приложения,
 // или что не относится к Vue приложению и UI
