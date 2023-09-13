f493d56084118e91f12881cd93457902bb9417e1
NinjiClaw <andyv@inbox.ru>
2023-09-13 16:46:35 +0500

    переделал postImage

--- src/api/imageApi.js
+++ src/api/imageApi.js
@@ -6,15 +6,5 @@ import { httpClient } from './httpClient/httpClient.js';
  * @returns {Promise<ResultContainer<ImageDto>>}
  */
 export function postImage(file) {
-  const formData = new FormData();
-  formData.append('image', file);
-
-  return httpClient.post('/images/upload', formData)
-  .then((response) => {
-      const imageDto = response.data;
-      console.log(imageDto);
-  })
-  .catch((error) => {
-      console.log(error);
-  })
+  return httpClient.post('/images/upload', file);
 }
