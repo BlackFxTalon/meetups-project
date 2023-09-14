f14950eaf14ba9a71225e7eca5d8ae5f359df2ef
NinjiClaw <andyv@inbox.ru>
2023-09-14 13:12:46 +0500

    TODO: UiLink.vue

--- src/components/UiLink.vue
+++ src/components/UiLink.vue
@@ -1,12 +1,19 @@
 <template>
-  <div><slot /> (Task 06-wrappers/01-UiLink)</div>
+  <component class="link" :is="tag">
+    <slot>Link!</slot>
+  </component>
 </template>
 
 <script>
-// TODO: Task 06-wrappers/01-UiLink
-
 export default {
   name: 'UiLink',
+
+  props: {
+    tag: {
+      type:[String, Object, Function],
+      default: 'router-link',
+    }
+  }
 };
 </script>
 
