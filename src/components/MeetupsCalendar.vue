7016443b30eb76eeda13bbe125cd91ba8082148d
NinjiClaw <andyv@inbox.ru>
2023-09-18 21:52:57 +0500

    TODO: MeetupsCalendar.vue

--- src/components/MeetupsCalendar.vue
+++ src/components/MeetupsCalendar.vue
@@ -1,12 +1,52 @@
 <template>
-  <div>Task 04-vue-cli/04-MeetupsCalendar + 10-slots/03-UiCalendarView</div>
+  <UiCalendarView v-slot="{ day }">
+    <UiCalendarEvent 
+    v-for="meetup in getMeetupsForDay[day.date]"
+    tag="a"
+    :key="meetup.id" 
+    :href="`/meetups/${meetup.id}`"
+    >
+      {{ meetup.title }}
+    </UiCalendarEvent>
+  </UiCalendarView>
 </template>
 
 <script>
-// TODO: Task 04-vue-cli/04-MeetupsCalendar + 10-slots/03-UiCalendarView
+import UiCalendarView from './UiCalendarView.vue';
+import UiCalendarEvent from './UiCalendarEvent.vue';
+import dayjs from "dayjs";
 
 export default {
   name: 'MeetupsCalendar',
+
+  components: {
+    UiCalendarEvent,
+    UiCalendarView,
+  },
+
+  props: {
+    meetups: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  computed: {
+   // Вычисляемое свойство для получения митапов для конкретного дня
+  getMeetupsForDay() {
+    const meetupsByDate = {};
+
+    for (const meetup of this.meetups) {
+        const formattedDate = dayjs(meetup.date).format("YYYY-MM-DD");
+        if (!meetupsByDate[formattedDate]) {
+          meetupsByDate[formattedDate] = [];
+        }
+        meetupsByDate[formattedDate].push(meetup);
+      }
+
+      return meetupsByDate;
+    },
+  }
 };
 </script>
 
