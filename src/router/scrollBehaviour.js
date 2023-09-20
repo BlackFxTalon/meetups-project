a562eb1e24af5966a5ca0b18ad2d5df6f37c8297
NinjiClaw <andyv@inbox.ru>
2023-09-21 01:04:53 +0500

    TODO: scrollBehaviour.js

--- src/router/scrollBehaviour.js
+++ src/router/scrollBehaviour.js
@@ -1,4 +1,21 @@
 // TODO: Task 05-vue-router/03-ScrollBehavior
 
 /** @implements {import('vue-router').RouterScrollBehavior} */
-export function scrollBehavior(to, from, savedPosition) {}
+export function scrollBehavior(to, from, savedPosition) {
+
+    if (to.meta.saveScrollPosition && from.meta.saveScrollPosition) {
+        return null; // Не изменять положение при переходе между маршрутами с истинными meta свойствами
+      }
+  
+      if (savedPosition) {
+        return savedPosition;
+      } 
+      
+      if(to.hash) {
+        return {
+          el: to.hash,
+        }
+      }
+
+      return { top: 0, left: 0 }
+}
