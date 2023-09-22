35a1a39a039725da0aff54eb726b4519765f6ed9
NinjiClaw <andyv@inbox.ru>
2023-09-22 21:59:12 +0500

    TODO: компосабл для отправки запросов (useApi.js)

--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -1,11 +1,6 @@
 import { ref } from 'vue';
-
-/*
-  TODO: Реализовать компосабл для отправки запросов
-        - Подразумевается, что в нём будут использоваться функции, возвращающие промис с ResultContainer
-        - Но вы можете использовать и другой подход
-        - Task composition/useApi
- */
+import { useToaster } from '../plugins/toaster/index.js';
+import { useProgress } from '../plugins/progress/index.js';
 
 /**
  * @template T
@@ -36,14 +31,71 @@ import { ref } from 'vue';
  * @return {IUseApiReturn<T, typeof apiFunc>}
  */
 export function useApi(apiFunc, { showProgress = false, successToast = false, errorToast = false } = {}) {
+
+  const toaster = useToaster();
+  const progress = useProgress();
+
+
+   /**
+   * @typedef {Function} DecoratedCallBack
+   */
+
+  /**
+   * Запустить асинхронную функцию с отображением прогресса
+   * @param {T} func
+   * @returns {T}
+   * @template {Function} T
+   */
+
+  function withProgress(func) {
+    return async (...args) => {
+      progress.start(func);
+      try {
+        const result = await func(...args);
+        progress.finish(func);
+        return result;
+      } catch (error) {
+        progress.fail();
+        throw error;
+      }
+    };
+  }
+
+   /**
+   * Показать тост по результату в ResultContainer
+   * @param {ResultContainer} result
+   * @returns {ResultContainer}
+   */
+
+   function toastResult(result) {
+    if (result.success && successToast) {
+      toaster.success(successToast === true ? result.data.message || result.data : successToast);
+    } else if (!result.success && errorToast) {
+      toaster.error(errorToast === true ? result.error.message : errorToast);
+    }
+  }
+
+
+
   const result = ref(null);
   const isLoading = ref(false);
 
-  const request = async (...args) => {};
+  const request = async (...args) => {
+
+    isLoading.value = true;
+    const requester = showProgress ? withProgress(apiFunc) : apiFunc;
+    result.value = await requester(...args);
+    isLoading.value = false;
+    toastResult(result.value);
+    return result.value;
+
+  };
 
   return {
     request,
     result,
     isLoading,
+    withProgress,
+    toastResult,
   };
 }
