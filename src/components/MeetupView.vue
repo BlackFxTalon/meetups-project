75311ae4aee005a9ed321329fc91d67470f300a5
NinjiClaw <andyv@inbox.ru>
2023-09-22 22:13:19 +0500

    TODO: MeetupView.vue

--- src/components/MeetupView.vue
+++ src/components/MeetupView.vue
@@ -9,14 +9,42 @@
         </div>
         <div class="meetup__aside">
           <MeetupInfo :organizer="meetup.organizer" :place="meetup.place" :date="meetup.date" />
-          <!-- TODO: Добавить проверку на аутентификацию и является ли пользователь организатором митапа -->
-          <!-- TODO: Реализовать кнопки (некоторые должны быть ссылками) -->
-          <div class="meetup__aside-buttons">
+          <div class="meetup__aside-buttons" v-if="isAuthenticated">
             <!-- TODO: Может добавить тут слот? -->
-            <UiButton variant="primary" class="meetup__aside-button">Редактировать</UiButton>
-            <UiButton variant="danger" class="meetup__aside-button">Удалить</UiButton>
-            <UiButton variant="secondary" class="meetup__aside-button">Отменить участие</UiButton>
-            <UiButton variant="primary" class="meetup__aside-button"> Участвовать </UiButton>
+            <UiButton 
+            variant="primary" 
+            class="meetup__aside-button"
+            tag="RouterLink"
+            :to="{ name: 'meetup-edit', params: { meetupId: meetup.id } }"
+            >
+            Редактировать
+            </UiButton>
+            <UiButton 
+            variant="danger" 
+            class="meetup__aside-button"
+            :disabled="deleteIsLoading"
+            @click="handleDeleteMeetup"
+            >
+            Удалить
+            </UiButton>
+            <UiButton 
+            variant="secondary" 
+            class="meetup__aside-button"
+            v-if="attending"
+            :disabled="toggleAttendIsLoading"
+            @click="handleAttendToggle"
+            >
+            Отменить участие
+            </UiButton>
+            <UiButton 
+            variant="primary" 
+            class="meetup__aside-button"
+            v-else
+            :disabled="toggleAttendIsLoading"
+            @click="handleAttendToggle"
+            > 
+            Участвовать 
+          </UiButton>
           </div>
         </div>
       </div>
@@ -29,6 +57,12 @@ import MeetupCover from './MeetupCover.vue';
 import MeetupInfo from './MeetupInfo.vue';
 import UiContainer from './UiContainer.vue';
 import UiButton from './UiButton.vue';
+import { computed, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { storeToRefs } from 'pinia';
+import { useAuthStore } from '../stores/useAuthStore.js';
+import { useApi } from '../composables/useApi.js';
+import { attendMeetup, deleteMeetup, leaveMeetup } from '../api/meetupsApi.js';
 
 export default {
   name: 'MeetupView',
@@ -47,15 +81,65 @@ export default {
     },
   },
 
-  setup() {
-    // TODO: Добавить обработку кнопок, включая работу с API
-    /*
-      TODO: Добавить тосты при успешных операциях
-            - Митап удалён
-            - Сохранено
-            - Текст ошибки в случае ошибки на API
-     */
-    // TODO: Будет плюсом блокировать кнопку на время загрузки
+  setup(props) {
+    const router = useRouter();
+    const { isAuthenticated } = storeToRefs(useAuthStore());
+
+    const { isLoading: deleteIsLoading, request: deleteMeetupRequest } = useApi(deleteMeetup, {
+      showProgress: true,
+      successToast: 'Митап удалён',
+      errorToast: true,
+    });
+    const { isLoading: attendIsLoading, request: attendMeetupRequest } = useApi(attendMeetup, {
+      showProgress: true,
+      successToast: 'Сохранено',
+      errorToast: true,
+    });
+    const { isLoading: leaveIsLoading, request: leaveMeetupRequest } = useApi(leaveMeetup, {
+      showProgress: true,
+      successToast: 'Сохранено',
+      errorToast: true,
+    });
+
+    const attending = ref(props.meetup.attending);
+
+    const toggleAttendIsLoading = computed(() => attendIsLoading.value || leaveIsLoading.value);
+
+    const handleDeleteMeetup = async () => {
+      if (confirm('Удалить митап?')) {
+        const result = await deleteMeetupRequest(props.meetup.id);
+        if (result.success) {
+          router.push({ name: 'meetups' });
+        }
+      }
+    };
+
+    const handleAttendToggle = async () => {
+      if (attending.value) {
+        const { success } = await leaveMeetupRequest(props.meetup.id);
+        if (success) {
+          attending.value = false;
+        }
+      } else {
+        const { success } = await attendMeetupRequest(props.meetup.id);
+        if (success) {
+          attending.value = true;
+        }
+      }
+    };
+
+    return {
+      router,
+      isAuthenticated,
+      deleteIsLoading,
+      deleteMeetupRequest,
+      attendMeetupRequest,
+      leaveMeetupRequest,
+      attending,
+      toggleAttendIsLoading,
+      handleDeleteMeetup,
+      handleAttendToggle,
+    }
   },
 };
 </script>
